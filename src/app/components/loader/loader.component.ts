5583f756b81ee1967587e5bb6ab1ce35a88937e9
mohamedalgharib26 <mohamedalgharib26@gmail.com>
2024-06-19 11:41:34 +0300

    add loader

---
+++ src/app/components/loader/loader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-Loader',
+  standalone: true,
+  imports: [],
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.scss',
+})
+export class LoaderComponent {}
--- src/app/layout/login/login.component.ts
+++ src/app/layout/login/login.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoaderComponent } from '../../components/loader/loader.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoaderComponent, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  isLoading!: boolean;
+  ngOnInit(): void {
+    this.loadContentAfterDelay();
+    this.loadVideo();
+  }
+
+  loadVideo() {
+    const video = document.querySelector(
+      '.background-video'
+    ) as HTMLVideoElement;
+    video.onloadeddata = () => {
+      this.isLoading = false;
+    };
+  }
+
+  loadContentAfterDelay() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 4000); // 4000 milliseconds = 4 seconds
+  }
+}
